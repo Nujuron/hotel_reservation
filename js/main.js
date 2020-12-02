76cb687d4aa0fcd38a4e49b6baaeb40c9d87cc97
2dawaa <2dawaa@informatica.edu>
2020-12-02 11:41:42 +0000

    master

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ function todayDate(){
 //set min date to today's date
 document.getElementById("when").setAttribute("min",todayDate);
 document.getElementById("when").setAttribute("placeholder",todayDate);
+
+room = {
+    adult : 0,
+    child : {
+        number : 0,
+        age : 0
+    }
+}
